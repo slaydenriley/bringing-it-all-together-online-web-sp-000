87c87907e46cce391a542ea26883b94e36c18e8e
Riley Slayden <slaydenriley@gmail.com>
2020-01-15 15:52:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -103,4 +103,9 @@ class Dog
     end.first
   end
 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 end
