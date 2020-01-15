3e652bc3328eef1c74614eba9e09908024614357
Riley Slayden <slaydenriley@gmail.com>
2020-01-15 15:20:48 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    "CREATE TABLE IF EXISTS dogs"
+  end
+
+end
