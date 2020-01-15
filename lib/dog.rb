6a10e4ffc67971be245111ca5300d383252419ac
Riley Slayden <slaydenriley@gmail.com>
2020-01-15 15:36:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    selfs
+    self
   end
 
   def self.create(name:, breed:)
